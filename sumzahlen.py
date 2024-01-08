309c85f072b1d77dcc55aa4ffc15f7ac6a192827
Tatiana Shuvaeva <talisman1982@gmail.com>
2024-01-08 10:57:59 +0000

    Sum Zahlen

--- sumzahlen.py
+++ sumzahlen.py
@@ -1,2 +1,13 @@
-a=[int(i) for i in input().split()]
-print(sum(a))
+s=[int(i) for i in input().split()]
+sum = 0
+for i in range(len(s)):                  
+    if i==0 and len(s)-1>=1:             
+        sum = s[i+1] + s[-1]                 
+    elif i==len(s)-1 and len(s)-1>=1:    
+        sum = s[i-1] + s[0]                  
+    elif len(s)==1:                      
+        sum = s[i]                           
+    else:                                
+        sum = s[i-1] + s[i+1]                
+    print(sum, end=" ")
+ 
