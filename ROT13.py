30a2cbc6036d8eafecdc5afbae98c5f7c957fdfb
Tatiana Shuvaeva <talisman1982@gmail.com>
2024-01-16 10:33:27 +0000

    Zwischenversion Caesar Verschlüsselung

--- ROT13.py
+++ ROT13.py
@@ -3,12 +3,16 @@ def rotate_word(word,n):
     for c in word:  
         code = ord(c)
         new_code = ord(c) + number
+        if new_code > ord('z'):
+            new_code -= 26
+        if new_code < ord('a'):
+            new_code += 26
         new_letter = chr(new_code)
         result += new_letter
     return result
 
-word = 'cheer'
-number = 7
+word = 'kayak'
+number = 13
 new_word = rotate_word(word,number)
 print(new_word)
    
