4a44d5efb54c5608d375275e0a613ffbd064afaf
Tatiana Shuvaeva <talisman1982@gmail.com>
2024-01-18 14:08:50 +0000

    Prozent berechnen (mit has no e)

--- chapter_9/has_no_e.py
+++ chapter_9/has_no_e.py
@@ -6,30 +6,12 @@ current_directory = os.path.realpath(
 path_to_file = os.path.join(current_directory, 'words.txt')
 file_input = open(path_to_file)
 
-
-# def has_no_letter(file_input, letter):
-#     wort_zahl_ohne = 0
-#     wort_zahl_mit = 0
-#     for line in file_input:
-#         if line.count(letter) == 0:
-#              wort_zahl_ohne += 1
-#              return line
-#         else:
-#             wort_zahl_mit += 1
-#             sum = wort_zahl_ohne + wort_zahl_mit
-#     prozent = wort_zahl_ohne/sum * 100
-#     return prozent
-
-# letter ='e'
-# words_ohne = has_no_letter(file_input, letter)
-# print(words_ohne)
-
-def has_no_e(file_input):
+def calculate_prozent(file_input):
     wort_zahl_ohne = 0
     wort_zahl_mit = 0
     sum = 0
     for line in file_input:
-        if line.count('e') == 0:
+        if has_no_e(line) == False:
              wort_zahl_ohne += 1
              print(line)
         else:
@@ -38,7 +20,10 @@ def has_no_e(file_input):
     prozent = wort_zahl_ohne/sum * 100
     return prozent
 
-prozent = has_no_e(file_input)
+def has_no_e(wort):
+    return wort.count('e')
+
+prozent = calculate_prozent(file_input)
 
 
-print(prozent) #33,074
+print(prozent) 
