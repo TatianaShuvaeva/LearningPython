33f37075e4bb5770187b646d7f67294b7738ad85
TatianaShuvaeva <talisman1982@gmail.com>
2024-03-08 12:44:04 +0100

    In_Fahrenheit

---
+++ Unterricht_Aufgaben/In_Fahrenheit.py
@@ -0,0 +1,27 @@
+Celsius = int(input('Bitte die Temperatur in Celsius eingeben '))
+Fahrenheit = (Celsius * 9/5)+32
+# print(Fahrenheit)
+print(f"{Fahrenheit}°F entspricht {Celsius}°C")
+
+Fahrenheit = int(input('Bitte die Temperatur in Fahrenheit eingeben '))
+Celsius = (Fahrenheit-32) * 5/9
+print(f"{Fahrenheit}°F entspricht {Celsius}°C")
+
+
+# print(round(Celsius,1))
+
+
+# def Celsius_in_Fahrenheit(Celsius):
+#     fahrenheit = (Celsius *9/5)+32
+#     return fahrenheit
+
+
+# print(Celsius_in_Fahrenheit(30))
+
+
+# def Fahrenheit_in_Celsius(fahrenheit):
+#     celsius = (fahrenheit-32) *5/9
+#     return celsius
+
+
+# print(Fahrenheit_in_Celsius(110))
