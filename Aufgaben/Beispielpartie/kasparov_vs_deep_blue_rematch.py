272ed97188e0e5f5d2df7e35a96e1593643b2509
Tatiana Shuvaeva <talisman1982@gmail.com>
2024-04-09 18:47:30 +0000

    Kasparov vs deep blue: Wörterbuch hinzugefügt

--- Aufgaben/Beispielpartie/kasparov_vs_deep_blue_rematch.py
+++ Aufgaben/Beispielpartie/kasparov_vs_deep_blue_rematch.py
@@ -8,5 +8,12 @@ path_to_file = os.path.join(ordnerpfad, 'daten.txt')
 print(path_to_file)
 
 file_input = open(path_to_file)
+metadaten = {}
 for line in file_input:
+    line_neu = line.strip('][\n')
+    line_split = line_neu.split(' ', maxsplit=1)
+    key = line_split[0]
+    value = line_split[1].strip('"')
+    metadaten[key] = value
 
+print(metadaten)
