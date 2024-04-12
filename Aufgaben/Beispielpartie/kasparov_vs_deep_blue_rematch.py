f485433e63c2656ce3b36fd5edda2546f60e2556
TatianaShuvaeva <talisman1982@gmail.com>
2024-04-12 13:31:41 +0200

    Kasparov vs deep blue refined

--- Aufgaben/Beispielpartie/kasparov_vs_deep_blue_rematch.py
+++ Aufgaben/Beispielpartie/kasparov_vs_deep_blue_rematch.py
@@ -15,26 +15,49 @@ line_number = 1
 games = ''
 for line in file_input:
     if line_number < 10:
-        line_neu = line.strip('][\n')
-        line_split = line_neu.split(' ', maxsplit=1)
+        line_neu = line.strip('][\n') #Entfernt man eckige Klammern und Zeilenumbrüche
+        line_split = line_neu.split(' ', maxsplit=1) #Teilt man die Zeichenfolge mit Leerzeichen als Trennzeichen auf
         key = line_split[0]
         value = line_split[1].strip('"')
         metadaten[key] = value
     elif line_number > 10:
-        print(line)
-        games += line
+        games_ohne = line.strip('\n') #Entfernt man Zeilenumbrüche
+        # games += games_ohne
+        games = games + ' ' + games_ohne   #Fügt man die Zeilen zu einer zusammen und fügt man ein Leerzeichen dazwischen ein'
 
     line_number += 1
+    
+games_split = games.split() #Teilt man eine Zeile mit einem Leerzeichen
 
-# games = '1.e4 c6 2.d4 d5 3.Nc3 dxe4 4.Nxe4 Nd7 5.Ng5 Ngf6 6.Bd3 e6 7.N1f3 h6 8.Nxe6 Qe7 9.O-O fxe6 10.Bg6+ Kd8 {Kasparov schüttelt kurz den Kopf} 11.Bf4 b5 12.a4 Bb7 13.Re1 Nd5 14.Bg3 Kc8 15.axb5 cxb5 16.Qd3 Bc6 17.Bf5 exf5 18.Rxe7 Bxe7 19.c4 1-0'
-# games_split = games.split(' ')
-# games_split =   ['1.e4', 'c6', '2.d4', 'd5', '3.Nc3', 'dxe4', '4.Nxe4', 'Nd7', '5.Ng5', 'Ngf6', '6.Bd3', 'e6', '7.N1f3', 'h6', '8.Nxe6', 'Qe7', '9.O-O', 'fxe6', '10.Bg6+', 'Kd8', '{Kasparov', 'schüttelt', 'kurz', 'den', 'Kopf}', '11.Bf4', 'b5', '12.a4', 'Bb7', '13.Re1', 'Nd5', '14.Bg3', 'Kc8', '15.axb5', 'cxb5', '16.Qd3', 'Bc6', '17.Bf5', 'exf5', '18.Rxe7', 'Bxe7', '19.c4', '1-0']
-# 'A'.isdigit()
-# games_refined = ['1.e4', 'c6', '2.d4', 'd5', '3.Nc3', 'dxe4', '4.Nxe4', 'Nd7', '5.Ng5', 'Ngf6', '6.Bd3', 'e6', '7.N1f3', 'h6', '8.Nxe6', 'Qe7', '9.O-O', 'fxe6', '10.Bg6+', 'Kd8', '11.Bf4', 'b5', '12.a4', 'Bb7', '13.Re1', 'Nd5', '14.Bg3', 'Kc8', '15.axb5', 'cxb5', '16.Qd3', 'Bc6', '17.Bf5', 'exf5', '18.Rxe7', 'Bxe7', '19.c4']
-# >>> '1.e4'.split('.')
+
+games_refined = ''
+for i in games_split:
+    for s in i:
+        if  s.isdigit() is True:    #Überprüft man, ob es Ziffern in jedem Elementgibt
+            games_refined = games_refined + ' '+ i # Entfernt man {Kasparov schüttelt kurz den Kopf}
+            break
+    
+# for e in games_refined: 
+games_element = games_refined.split()
+# print(games_element)
+# zuege_spiel = []
+# for i in games_element:
+#     igames_element = i.split('.')
+#     for i in games_element:
+#         if len(i) == 2:
+#             zuege_spiel = zuege_spiel + ' ' + i[1]
+#         else:
+#             zuege_spiel = zuege_spiel + ' ' + i[0]
+
+    
+#     print(zuege_spiel)
+# print(metadaten)
+    #  igames_element = i.split('.')
+
+#  '1.e4'.split('.')
 # ['1', 'e4']
-# >>> 'c6'.split('.')
+# 'c6'.split('.')
 # ['c6']
-games_result = ['e4']
+# games_result = ['e4']
 # print(metadaten)
 
