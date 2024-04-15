41b34a1e45fa4c97c42e5310802d130816703e6b
TatianaShuvaeva <talisman1982@gmail.com>
2024-04-15 14:57:21 +0200

    Widerstand

---
+++ Unterricht_Aufgaben/Widerstand.py
@@ -0,0 +1,5 @@
+spannung = float(input("Bitte geben Sie die elektrische Spannung ein: "))
+strom = float(input("Bitte geben Sie die elektrische Stromstärke ein: "))
+ 
+widerstand = spannung / strom
+print(f"Der elektrische Widerstand beträgt {widerstand} Ohm.")
