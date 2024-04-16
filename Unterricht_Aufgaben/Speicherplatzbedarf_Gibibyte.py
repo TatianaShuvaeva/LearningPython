de8e4515be0b34fa44fd4c123be569364a2525e3
TatianaShuvaeva <talisman1982@gmail.com>
2024-04-16 13:29:44 +0200

    Speicherplatzbedarfneu

--- Unterricht_Aufgaben/Speicherplatzbedarf_Gibibyte.py
+++ Unterricht_Aufgaben/Speicherplatzbedarf_Gibibyte.py
@@ -1,19 +1,22 @@
-# höhe = int(input("Bitte geben Sie die Höhe des Bildes ein: "))
-# if höhe <=0: 
-#     print(f"Ihre Zahl: {höhe} sollte eine positive Zahl sein")
-
-# breite = int(input("Bitte geben Sie die Breite des Bildes ein: "))
-# if breite <=0: 
-#     print(f"Ihre Zahl: {breite} sollte eine positive Zahl sein")
+höhe = int(input("Bitte geben Sie die Höhe des Bildes ein: "))
+if höhe <=0: 
+    print(f"Ihre Zahl: {höhe} sollte eine positive Zahl sein")
+    höhe = int(input("Bitte geben Sie die Höhe des Bildes ein: "))
+    
+breite = int(input("Bitte geben Sie die Breite des Bildes ein: "))
+if breite <=0: 
+    print(f"Ihre Zahl: {breite} sollte eine positive Zahl sein")
+    breite = int(input("Bitte geben Sie die Breite des Bildes ein: "))
     
 speicherbedarf = int(input("Bitte geben Sie den Speicherbedarf ein: "))
 if speicherbedarf <=0: 
     print(f"Ihre Zahl: {speicherbedarf} sollte eine positive Zahl sein")
+    speicherbedarf = int(input("Bitte geben Sie den Speicherbedarf ein: "))
     
 anzahl_bilder = int(input("Bitte geben Sie die Anzahl der Bilder ein: "))
 if  anzahl_bilder <=0: 
     print(f"Ihre Zahl: {anzahl_bilder} sollte eine positive Zahl sein")
- 
+    anzahl_bilder = int(input("Bitte geben Sie die Anzahl der Bilder ein: "))
 
 speicherbedarf_byte = breite * höhe * speicherbedarf * anzahl_bilder
 speicherplatzbedarf_gibibyte = speicherbedarf_byte / (1024 ** 3)  # Umrechnung in Gibibyte
