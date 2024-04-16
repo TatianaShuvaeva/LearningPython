edd03a5faa3edab426a2fdd144150be02b66268d
TatianaShuvaeva <talisman1982@gmail.com>
2024-04-16 13:29:11 +0200

    Durchschnittverbrauchneu

--- Unterricht_Aufgaben/Durchschnittverbrauch Kraftstoff.py
+++ Unterricht_Aufgaben/Durchschnittverbrauch Kraftstoff.py
@@ -1,4 +1,4 @@
-# kraftstoffmenge = float(input("Bitte geben Sie die getankte menge ein: "))
+# kraftstoffmenge = float(input("Bitte geben Sie die kraftstoffmenge ein: "))
 # kilometer = float(input("Bitte geben Sie die gefahrenen Kilometer ein: "))
 
 # durchschnittverbrauch = (kraftstoffmenge/kilometer) * 100
@@ -6,13 +6,15 @@
 # print(f"Durchschnittverbrauch:{durchschnittverbrauch} Liter/100 km")
 
 
-kraftstoffmenge = float(input("Bitte geben Sie die getankte menge ein: "))
+kraftstoffmenge = float(input("Bitte geben Sie die kraftstoffmenge ein: "))
 if kraftstoffmenge <= 0:
     print(f"Ihre Zahl: {kraftstoffmenge} sollte eine positive Zahl sein")
-
+    kraftstoffmenge = float(input("Bitte geben Sie die kraftstoffmenge ein: "))
+    
 kilometer = float(input("Bitte geben Sie die gefahrenen Kilometer ein: "))
 if kilometer <= 0:
     print(f"Ihre Zahl: {kilometer} sollte eine positive Zahl sein")
+    kilometer = float(input("Bitte geben Sie die gefahrenen Kilometer ein: "))
     
 durchschnittverbrauch = (kraftstoffmenge/kilometer) * 100
 print(f"Durchschnittverbrauch:{durchschnittverbrauch} Liter/100 km")
