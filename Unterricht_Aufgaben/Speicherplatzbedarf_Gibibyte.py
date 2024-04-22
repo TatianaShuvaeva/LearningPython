0d6ad2cf18c65e5b2247672f21f47331a8410bad
TatianaShuvaeva <talisman1982@gmail.com>
2024-04-22 14:51:00 +0200

    Speicherplatzbedarf Gibibyte neu

--- Unterricht_Aufgaben/Speicherplatzbedarf_Gibibyte.py
+++ Unterricht_Aufgaben/Speicherplatzbedarf_Gibibyte.py
@@ -1,22 +1,26 @@
-höhe = int(input("Bitte geben Sie die Höhe des Bildes ein: "))
-if höhe <=0: 
-    print(f"Ihre Zahl: {höhe} sollte eine positive Zahl sein")
-    höhe = int(input("Bitte geben Sie die Höhe des Bildes ein: "))
+höhe = 0
+breite = 0
+speicherbedarf = 0
+anzahl_bilder = 0
+
+while höhe <= 0 or breite <= 0 or speicherbedarf <=0 or anzahl_bilder <=0:  # Beginn einer Schleife, die nur durch ein break beendet
+
+    if höhe <= 0:  # Überprüfung, ob gültig ist
+        print("Die Höhe des Bildes sollte > 0 sein")
+        höhe = int(input("Bitte geben Sie die Höhe des Bildes ein: "))
+        
+    if breite <= 0:  # Überprüfung, ob die elektrische Stromstärker gültig sind
+        print("Die Breite des Bildes sollten > 0 sein")
+        breite = int(input("Bitte geben Sie die Breite des Bildes ein: "))
     
-breite = int(input("Bitte geben Sie die Breite des Bildes ein: "))
-if breite <=0: 
-    print(f"Ihre Zahl: {breite} sollte eine positive Zahl sein")
-    breite = int(input("Bitte geben Sie die Breite des Bildes ein: "))
-    
-speicherbedarf = int(input("Bitte geben Sie den Speicherbedarf ein: "))
-if speicherbedarf <=0: 
-    print(f"Ihre Zahl: {speicherbedarf} sollte eine positive Zahl sein")
-    speicherbedarf = int(input("Bitte geben Sie den Speicherbedarf ein: "))
-    
-anzahl_bilder = int(input("Bitte geben Sie die Anzahl der Bilder ein: "))
-if  anzahl_bilder <=0: 
-    print(f"Ihre Zahl: {anzahl_bilder} sollte eine positive Zahl sein")
-    anzahl_bilder = int(input("Bitte geben Sie die Anzahl der Bilder ein: "))
+    if speicherbedarf <=0:
+        print("Der Speicherbedarf sollten > 0 sein")
+        speicherbedarf = int(input("Bitte geben Sie den Speicherbedarf ein: "))
+        
+    if  anzahl_bilder <=0: 
+        print("Die Anzahl der Bilder sollten > 0 sein")
+        anzahl_bilder = int(input("Bitte geben Sie die Anzahl der Bilder ein: "))    
+        
 
 speicherbedarf_byte = breite * höhe * speicherbedarf * anzahl_bilder
 speicherplatzbedarf_gibibyte = speicherbedarf_byte / (1024 ** 3)  # Umrechnung in Gibibyte
