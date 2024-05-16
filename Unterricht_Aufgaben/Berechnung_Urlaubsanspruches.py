b9d45fae0fe412edadef4a62eb256b2759708388
TatianaShuvaeva <talisman1982@gmail.com>
2024-05-16 13:21:39 +0200

    Berechnung Urlaubsanspruches neu

--- Unterricht_Aufgaben/Berechnung_Urlaubsanspruches.py
+++ Unterricht_Aufgaben/Berechnung_Urlaubsanspruches.py
@@ -46,41 +46,41 @@
 
 #Aufgabe 3
 
-# behinderung = int(input("Geben Sie bitte ein, wie viel Prozent beträgt die Behinderung: "))
-# alter_beschaeftigten = int(input("Geben Sie bitte Alter der Beschäftigte ein: "))
-# urlaub = 30
-
-# if alter_beschaeftigten > 55: # alter_beschaeftigten >= 55 muss sein
-#         urlaub = 32
-# else:
-#     if alter_beschaeftigten < 18:
-#         urlaub = 35
-#     else:
-#         urlaub = 30  
-      
-
-# if behinderung >= 50:
-#     urlaub +=  5
-     
-# print(urlaub)
-
-
-#Aufgabe 4
-
 behinderung = int(input("Geben Sie bitte ein, wie viel Prozent beträgt die Behinderung: "))
 alter_beschaeftigten = int(input("Geben Sie bitte Alter der Beschäftigte ein: "))
 urlaub = 30
 
-if alter_beschaeftigten < 18:
-   urlaub = 35 
-
-if alter_beschaeftigten <= 55: # alter_beschaeftigten < 55 muss sein, 
-                               # fälschlicherweise für alle Beschäftigte unter 18 Jahren berechnet, urlaub= 30 wird sein
-    urlaub = 30 
+if alter_beschaeftigten > 55: # alter_beschaeftigten >= 55 muss sein
+        urlaub = 32
 else:
-    urlaub = 32
-     
+    if alter_beschaeftigten < 18:
+        urlaub = 35
+    else:
+        urlaub = 30  
+      
+
 if behinderung >= 50:
     urlaub +=  5
      
 print(urlaub)
+
+
+#Aufgabe 4
+
+# behinderung = int(input("Geben Sie bitte ein, wie viel Prozent beträgt die Behinderung: "))
+# alter_beschaeftigten = int(input("Geben Sie bitte Alter der Beschäftigte ein: "))
+# urlaub = 30
+
+# if alter_beschaeftigten < 18:
+#    urlaub = 35 
+
+# if alter_beschaeftigten <= 55: # alter_beschaeftigten < 55 muss sein, 
+#                                # fälschlicherweise für alle Beschäftigte unter 18 Jahren berechnet, urlaub= 30 wird sein
+#     urlaub = 30 
+# else:
+#     urlaub = 32
+     
+# if behinderung >= 50:
+#     urlaub +=  5
+     
+# print(urlaub)
