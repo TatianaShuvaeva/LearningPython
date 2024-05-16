877659db87cea4e73487a5acf7900c9feeb230e1
TatianaShuvaeva <talisman1982@gmail.com>
2024-05-16 14:48:58 +0200

    Wellnes Hotel Entscheidungstabelle2

--- Unterricht_Aufgaben/wellnes_hotel.py
+++ Unterricht_Aufgaben/wellnes_hotel.py
@@ -3,30 +3,54 @@ eingabe_drei_uebernachtungen = str(input("Willst Du drei oder mehr Übernachtung
 eingabe_vollpension = str(input("Willst Du Vollpension. Gib bitte ja oder nein ein: "))
 
 if eingabe_wellness == 'ja' and eingabe_drei_uebernachtungen == 'ja' and eingabe_vollpension == 'ja':
-    print("Früstück mit Abendbrot, Mittagessen, 5 Freigetränke, freie Nutzung des Fitnessraums, Saunabesuch gratis, eine Massage gratis  gebucht")
+    print("Frühstück mit Abendbrot, Mittagessen, 5 Freigetränke, freie Nutzung des Fitnessraums, Saunabesuch gratis, eine Massage gratis  gebucht")
 
 elif eingabe_wellness == 'ja' and eingabe_drei_uebernachtungen == 'ja' and eingabe_vollpension == 'nein':
-    print("Früstück mit Abendbrot, 5 Freigetränke, freie Nutzung des Fitnessraums, Saunabesuch gratis, eine Massage gratis  gebucht")
+    print("Frühstück mit Abendbrot, 5 Freigetränke, freie Nutzung des Fitnessraums, Saunabesuch gratis, eine Massage gratis  gebucht")
     
 elif eingabe_wellness == 'ja' and eingabe_drei_uebernachtungen == 'ja' and eingabe_vollpension == 'nein':
-    print("Früstück mit Abendbrot, 5 Freigetränke, freie Nutzung des Fitnessraums, Saunabesuch gratis, eine Massage gratis  gebucht")
+    print("Frühstück mit Abendbrot, 5 Freigetränke, freie Nutzung des Fitnessraums, Saunabesuch gratis, eine Massage gratis  gebucht")
 
 elif eingabe_wellness == 'ja' and eingabe_drei_uebernachtungen == 'nein' and eingabe_vollpension == 'ja':
-    print("Früstück mit Abendbrot, Mittagessen, 5 Freigetränke, freie Nutzung des Fitnessraums, Saunabesuch gratis, eine Massage gratis  gebucht")
+    print("Frühstück mit Abendbrot, Mittagessen, 5 Freigetränke, freie Nutzung des Fitnessraums, Saunabesuch gratis, eine Massage gratis  gebucht")
     
 elif eingabe_wellness == 'ja' and eingabe_drei_uebernachtungen == 'nein' and eingabe_vollpension == 'nein':
-    print("Früstück mit Abendbrot, 5 Freigetränke, freie Nutzung des Fitnessraums, Saunabesuch gratis, eine Massage gratis  gebucht")
+    print("Frühstück mit Abendbrot, 5 Freigetränke, freie Nutzung des Fitnessraums, Saunabesuch gratis, eine Massage gratis  gebucht")
 
 elif eingabe_wellness == 'nein' and eingabe_drei_uebernachtungen == 'ja' and eingabe_vollpension == 'ja':
-    print("Früstück mit Abendbrot, Mittagessen, 5 Freigetränke, freie Nutzung des Fitnessraums, Saunabesuch gratis gebucht")
+    print("Frühstück mit Abendbrot, Mittagessen, 5 Freigetränke, freie Nutzung des Fitnessraums, Saunabesuch gratis gebucht")
 
 elif eingabe_wellness == 'nein' and eingabe_drei_uebernachtungen == 'ja' and eingabe_vollpension == 'nein':
-    print("Früstück mit Abendbrot, freie Nutzung des Fitnessraums, Saunabesuch gratis  gebucht")
+    print("Frühstück mit Abendbrot, freie Nutzung des Fitnessraums, Saunabesuch gratis  gebucht")
 
 elif eingabe_wellness == 'nein' and eingabe_drei_uebernachtungen == 'nein' and eingabe_vollpension == 'ja':
-    print("Früstück mit Abendbrot, Mittagessen, 5 Freigetränke, freie Nutzung des Fitnessraums  gebucht")
+    print("Frühstück mit Abendbrot, Mittagessen, 5 Freigetränke, freie Nutzung des Fitnessraums  gebucht")
     
 elif eingabe_wellness == 'nein' and eingabe_drei_uebernachtungen == 'nein' and eingabe_vollpension == 'nein':
-    print("Früstück mit Abendbrot, Mittagessen, 5 Freigetränke, freie Nutzung des Fitnessraums  gebucht")
+    print("Frühstück mit Abendbrot, Mittagessen, 5 Freigetränke, freie Nutzung des Fitnessraums  gebucht")
+
+
+
+
+
+# if eingabe_wellness == 'ja':
+#     if eingabe_drei_uebernachtungen == 'ja' and eingabe_vollpension == 'ja':
+#         print("Frühstück mit Abendbrot, Mittagessen, 5 Freigetränke, freie Nutzung des Fitnessraums, Saunabesuch gratis, eine Massage gratis gebucht")
+#     elif eingabe_drei_uebernachtungen == 'ja' and eingabe_vollpension == 'nein':
+#         print("Frühstück mit Abendbrot, 5 Freigetränke, freie Nutzung des Fitnessraums, Saunabesuch gratis, eine Massage gratis gebucht")
+#     elif eingabe_drei_uebernachtungen == 'nein' and eingabe_vollpension == 'ja':
+#         print("Frühstück mit Abendbrot, Mittagessen, 5 Freigetränke, freie Nutzung des Fitnessraums, Saunabesuch gratis gebucht")
+#     elif eingabe_drei_uebernachtungen == 'nein' and eingabe_vollpension == 'nein':
+#         print("Frühstück mit Abendbrot, 5 Freigetränke, freie Nutzung des Fitnessraums, Saunabesuch gratis gebucht")
+# else:
+#     if eingabe_drei_uebernachtungen == 'ja' and eingabe_vollpension == 'ja':
+#         print("Frühstück mit Abendbrot, Mittagessen, 5 Freigetränke, freie Nutzung des Fitnessraums gebucht")
+#     elif eingabe_drei_uebernachtungen == 'ja' and eingabe_vollpension == 'nein':
+#         print("Frühstück mit Abendbrot, freie Nutzung des Fitnessraums, Saunabesuch gratis gebucht")
+#     elif eingabe_drei_uebernachtungen == 'nein' and eingabe_vollpension == 'ja':
+#         print("Frühstück mit Abendbrot, Mittagessen, 5 Freigetränke, freie Nutzung des Fitnessraums gebucht")
+#     elif eingabe_drei_uebernachtungen == 'nein' and eingabe_vollpension == 'nein':
+#         print("Frühstück mit Abendbrot, Mittagessen, 5 Freigetränke, freie Nutzung des Fitnessraums gebucht")
+
 
 
