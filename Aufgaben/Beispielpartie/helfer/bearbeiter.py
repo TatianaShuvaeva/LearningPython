f1762e0f3b1da9d127fab303b5bec6d3ffa80f70
TatianaShuvaeva <talisman1982@gmail.com>
2024-05-20 10:31:30 +0000

    Kasparov mit while ohne Anzahl Zeile

--- Aufgaben/Beispielpartie/helfer/bearbeiter.py
+++ Aufgaben/Beispielpartie/helfer/bearbeiter.py
@@ -7,7 +7,6 @@ class Bearbeiter:
         metadaten = {}
         zuege_spiel = []
 
-        line_number = 1
         games = ''
         line = file_input.readline()
         while line != '\n':
@@ -17,47 +16,38 @@ class Bearbeiter:
             value = line_split[1].strip('"')
             metadaten[key] = value
             line = file_input.readline()          
-                
-        line = file_input.readline()
-        line = file_input.readline()
-        line = file_input.readline()
-        line = file_input.readline()
-        line = file_input.readline()
-        line = file_input.readline()
-        line = file_input.readline()
-        #     elif line_number > 10:
-        games_ohne = line.strip('\n')  # Entfernt man Zeilenumbrüche
-
-        # Fügt man die Zeilen zu einer zusammen und fügt man ein Leerzeichen
-        # dazwischen ein'
-        games = games + ' ' + games_ohne
-
-        line_number += 1
-        print(metadaten)   
-        # games_split = games.split()  # Teilt man eine Zeile mit einem Leerzeichen
-
-
-        # games_refined = []
-        # for element in games_split[:-1]:  # Alle Elemente außer dem letzten
-        #     for symbol in element:
-        #         if symbol.isdigit() is True:  # Überprüft man, ob es Ziffern in jedem Element gibt
-        #             games_refined.append(element)  # Entfernt man {Kasparov schüttelt kurz den Kopf}
-        #             break
+        
+        while line != '':        
+            line = file_input.readline()
+            games_ohne = line.strip('\n')  # Entfernt man Zeilenumbrüche
             
-        # # for e in games_refined: 
+            # Fügt man die Zeilen zu einer zusammen und fügt man ein Leerzeichen
+            # dazwischen ein'
+            games = games + ' ' + games_ohne
+        
+        games_split = games.split()  # Teilt man eine Zeile mit einem Leerzeichen
 
-        # for element in games_refined:
-        #     igames_element = element.split('.')  # Element nach Punkt aufteilen
+        games_refined = []
+        for element in games_split[:-1]:  # Alle Elemente außer dem letzten
+            for symbol in element:
+                if symbol.isdigit() is True:  # Überprüft man, ob es Ziffern in jedem Element gibt
+                    games_refined.append(element)  # Entfernt man {Kasparov schüttelt kurz den Kopf}
+                    break
             
-        #     #  Fügt man dem neuen Array nur zweite Element hinzu, wenn es zwei davon gibt
+        for element in games_refined:
+            igames_element = element.split('.')  # Element nach Punkt aufteilen
             
-        #     if len(igames_element) == 2:
-        #         zuege_spiel.append(igames_element[1]) 
-        #     else:
-        #         zuege_spiel.append(igames_element[0])
+            #  Fügt man dem neuen Array nur zweite Element hinzu, wenn es zwei davon gibt
+            
+            if len(igames_element) == 2:
+                zuege_spiel.append(igames_element[1]) 
+            else:
+                zuege_spiel.append(igames_element[0])
+        
+        print(metadaten)
+        
+        # for element in zuege_spiel:
+        #     print(f"zuege[{zuege_spiel.index(element)}]={element}")
 
-        # # for element in zuege_spiel:
-        # #     print(f"zuege[{zuege_spiel.index(element)}]={element}")
-
-        # for idx, element in enumerate(zuege_spiel):  # Zweite bessere Variante 
-        #     print(f"zuege[{idx}]={element}")
+        for idx, element in enumerate(zuege_spiel):  # Zweite bessere Variante 
+            print(f"zuege[{idx}]={element}")
