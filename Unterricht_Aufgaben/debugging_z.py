2937455926fd0a175c8ddcd3114b0d2ed85fec2c
TatianaShuvaeva <talisman1982@gmail.com>
2024-05-23 15:36:53 +0200

    Debuggin z

---
+++ Unterricht_Aufgaben/debugging_z.py
@@ -0,0 +1,49 @@
+# z = 0
+# for i in range(1,4):
+#     z *= 1
+# print(z) 
+
+# z = -2
+# i = 99
+# while i> 88:
+#     z += 3
+#     z -= z
+#     i -= 2 
+# print(z) 
+
+
+# z = 0
+# i = 0
+# while i< 20:
+#     j= i
+#     while j< 20:  
+#         z = z+ j
+#         j += 1
+#     i = i + 5
+# print(z) 
+
+
+# z = 4
+# i = 4
+# while i > 0:  
+#     z += 4
+#     z = z/i
+#     i = i - 1
+# print(z) 
+
+# z = 0
+# while z != 1 or z != 2:
+#     z = int(input("Geben Sie bitte die Zahl ein: "))
+    
+
+i = 10
+z = 100
+while i > 0:
+    if i == 0:
+        z -= i
+    else:
+        z += i
+i -= 1
+print(z)
+    
+    
