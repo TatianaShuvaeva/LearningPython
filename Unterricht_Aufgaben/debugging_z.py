fa47538b1e4939a9cc6bec8c45e0e79c5791de82
TatianaShuvaeva <talisman1982@gmail.com>
2024-05-24 09:43:43 +0200

    Debugging Z neu

--- Unterricht_Aufgaben/debugging_z.py
+++ Unterricht_Aufgaben/debugging_z.py
@@ -1,11 +1,11 @@
 # z = 0
-# for i in range(1,4):
+# for i in range(1,4): # 3 mal schleife (1,2,3), ergebnis = 0
 #     z *= 1
 # print(z) 
 
 # z = -2
 # i = 99
-# while i> 88:
+# while i> 88: # 6 mal schleife(99, 97, 95, 93, 91, 89)
 #     z += 3
 #     z -= z
 #     i -= 2 
@@ -31,19 +31,20 @@
 #     i = i - 1
 # print(z) 
 
-# z = 0
+z = 0
 # while z != 1 or z != 2: # True or False = True
-#     z = int(input("Geben Sie bitte die Zahl ein: "))
+while z != 1 and z != 2:
+    z = int(input("Geben Sie bitte die Zahl ein: "))
     
 
-i = 10
-z = 100
-while i > 0:
-    if i % 2 == 0:
-        z -= i
-    else:
-        z += i
-    i -= 1
-print(z)
+# i = 10
+# z = 100
+# while i > 0:
+#     if i % 2 == 0:
+#         z -= i
+#     else:
+#         z += i
+#     i -= 1
+# print(z)
     
     
