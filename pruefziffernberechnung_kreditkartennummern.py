a0a09db6f9d5d975f0f1ac605f06f2de984fcedd
TatianaShuvaeva <talisman1982@gmail.com>
2024-05-27 21:46:29 +0200

    Prüfziffernberechnung 1-2 Schritt

--- pruefziffernberechnung_kreditkartennummern.py
+++ pruefziffernberechnung_kreditkartennummern.py
@@ -1,18 +1,20 @@
 kreditkartennummer = '123456789'
-liste_verdoppelung= []
+liste_verdoppelung = []
 quersumme = 0
-#ziffern = [int(i) for i in kreditkartennummer.split()]
+# ziffern = [int(i) for i in kreditkartennummer.split()]
 list_ziffern = list(kreditkartennummer)
-for i in range(len(list_ziffern)-2,-1,-2):
+for i in range(len(list_ziffern)-2, -1, -2):
     a = int(list_ziffern[i])*2
     liste_verdoppelung.append(a)
-for i in liste_verdoppelung:
-    if liste_verdoppelung[i] < 9:
-        quersumme = liste_verdoppelung[i]
-    else:
-        for 
-
 print(liste_verdoppelung)
-#ziffern = [int(i) for i in kreditkartennummer.split()]
+
+for i in liste_verdoppelung[i]:
+    print(i)
+    element = liste_verdoppelung[i]
+    if element > 9:
+        element_in_str = str(element)
+        for i in element_in_str:
+            print(i)
 
 
+# ziffern = [int(i) for i in kreditkartennummer.split()]
