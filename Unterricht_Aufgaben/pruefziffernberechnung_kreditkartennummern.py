c8cd4d04413a498b08cd50f7930f3b0856212790
TatianaShuvaeva <talisman1982@gmail.com>
2024-05-28 14:09:46 +0200

    Prüfziffernberechnung 1-2 Schritte neu

---
+++ Unterricht_Aufgaben/pruefziffernberechnung_kreditkartennummern.py
@@ -0,0 +1,29 @@
+kreditkartennummer = '9342571866701996'
+liste_verdoppelung = []
+summe = 0
+quersumme = 0
+list_ziffern = list(kreditkartennummer)
+for i in range(len(list_ziffern)-2, -1, -2):
+    a = int(list_ziffern[i])*2
+    liste_verdoppelung.append(a)
+for i in range(1, len(list_ziffern)-2, 2): 
+    c = int(list_ziffern[i]) 
+    liste_verdoppelung.append(c)
+print(liste_verdoppelung)    
+
+
+for i in liste_verdoppelung:
+   
+    element = i
+    if element > 9:
+        element_in_str = str(element)
+        for i in range(len(element_in_str)):
+            b = int(element_in_str[i])
+            quersumme += b
+#print(summe_elementen)
+    else:
+        summe += i
+summe = summe + quersumme
+print(summe)
+
+
