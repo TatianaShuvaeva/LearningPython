1c8b0b4d40922cc42cb05939a1e6a036b5b7d676
TatianaShuvaeva <talisman1982@gmail.com>
2024-07-04 15:50:34 +0200

    Promillerechner

---
+++ Unterricht_Aufgaben/Promillerechner.py
@@ -0,0 +1,18 @@
+mensch = str(input("Geben Sie bitte ein, sind Sie Frau (f) oder Mann (m)?: "))
+masse = float(input("Geben Sie bitte Ihr Gewicht in kg ein:  "))
+volumen = float(input("Geben Sie bitte die Menge des konsumierten Alkohol in ml ein:  "))
+alkoholvolumenanteil = float(input("Geben Sie bitte den Alkoholgehalt Ihres Getränks in % ein:  "))
+r_mann = 0.7
+r_frau = 0.6
+dichte = 0.8
+a = volumen * alkoholvolumenanteil * dichte
+
+if mensch.lower() == "m":
+    c = round((a/(masse*r_mann)),2)
+    print(f'Ihr Promillewert beträgt {c}')
+elif mensch.lower() == "f":
+    c = round((a/(masse*r_frau)),2)
+    print(f'Ihr Promillewert beträgt {c}')
+else:
+    print(f'Sie haben falsch eingegeben, sind Sie Frau (f) oder Mann (m)?')
+
