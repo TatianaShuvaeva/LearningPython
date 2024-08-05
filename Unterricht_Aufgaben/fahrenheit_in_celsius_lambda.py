e6d9789de219587558b63413eb0ad4694031be62
TatianaShuvaeva <talisman1982@gmail.com>
2024-08-05 18:06:33 +0000

    Add Genauigkeit

--- Unterricht_Aufgaben/fahrenheit_in_celsius_lambda.py
+++ Unterricht_Aufgaben/fahrenheit_in_celsius_lambda.py
@@ -1,17 +1,17 @@
 # Aufgabe 1 Fahrenheit in Celsius
 
-# fahrenheit = [12,45,67, 89]
+fahrenheit = [12,45,67,89]
   
-# celsius = list(map((lambda i: (i-32) * 5/9), fahrenheit))
-# for i in celsius:
-#     print(round(i))
+celsius = list(map((lambda i: (i-32) * 5/9), fahrenheit))
+for i in celsius:
+    print(round(i,1))
  
 # Aufgabe 2 Celsius in Fahrenheit  
-# celsius = [12,45,67, 89]
+# celsius = [-11, 7, 19, 32]
   
 # fahrenheit = list(map(lambda i: (i * 9/5)+32, celsius))
 # for k in fahrenheit:
-#     print(round(k))
+#     print(round(k,1))
     
 # Aufgabe 3 Listenabstraktion (List Comprehension)
 
@@ -19,14 +19,14 @@
   
 # celsius = list(map((lambda i: (i-32) * 5/9), fahrenheit))
     
-# liste_celsius = [round(v) for v in celsius]
+# liste_celsius = [round(v,1) for v in celsius]
 # print(liste_celsius)
 
 
 # Aufgabe 4 Listenabstraktion (List Comprehension)
 
-celsius = [12,45,67, 89]
+# celsius = [12,45,67, 89]
   
-fahrenheit = list(map(lambda i: (i * 9/5)+32, celsius))
-liste_fahrenheit = [round(v) for v in fahrenheit]
-print(liste_fahrenheit)
+# fahrenheit = list(map(lambda i: (i * 9/5)+32, celsius))
+# liste_fahrenheit = [round(v,1) for v in fahrenheit]
+# print(liste_fahrenheit)
