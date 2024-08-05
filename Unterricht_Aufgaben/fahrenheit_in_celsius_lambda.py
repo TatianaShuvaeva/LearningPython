5a5a82e77980f014e22d56c1505f97a80d468c2c
TatianaShuvaeva <talisman1982@gmail.com>
2024-08-05 15:25:41 +0000

    Add Aufgaben 3 und 4 Listenabstraktion (List Comprehension)

--- Unterricht_Aufgaben/fahrenheit_in_celsius_lambda.py
+++ Unterricht_Aufgaben/fahrenheit_in_celsius_lambda.py
@@ -1,4 +1,4 @@
-# Fahrenheit in Celsius
+# Aufgabe 1 Fahrenheit in Celsius
 
 # fahrenheit = [12,45,67, 89]
   
@@ -6,9 +6,27 @@
 # for i in celsius:
 #     print(round(i))
  
-# Celsius in Fahrenheit  
+# Aufgabe 2 Celsius in Fahrenheit  
+# celsius = [12,45,67, 89]
+  
+# fahrenheit = list(map(lambda i: (i * 9/5)+32, celsius))
+# for k in fahrenheit:
+#     print(round(k))
+    
+# Aufgabe 3 Listenabstraktion (List Comprehension)
+
+# fahrenheit = [12,45,67, 89]
+  
+# celsius = list(map((lambda i: (i-32) * 5/9), fahrenheit))
+    
+# liste_celsius = [round(v) for v in celsius]
+# print(liste_celsius)
+
+
+# Aufgabe 4 Listenabstraktion (List Comprehension)
+
 celsius = [12,45,67, 89]
   
 fahrenheit = list(map(lambda i: (i * 9/5)+32, celsius))
-for k in fahrenheit:
-    print(round(k))
+liste_fahrenheit = [round(v) for v in fahrenheit]
+print(liste_fahrenheit)
