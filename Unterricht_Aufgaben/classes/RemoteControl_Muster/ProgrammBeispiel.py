ddf9107f14a6859f490783396a0adbd5cf560ede
TatianaShuvaeva <talisman1982@gmail.com>
2024-08-27 10:18:10 +0200

    private Methoden und  __str__ Methode hinzugefügt

--- Unterricht_Aufgaben/classes/RemoteControl_Muster/ProgrammBeispiel.py
+++ Unterricht_Aufgaben/classes/RemoteControl_Muster/ProgrammBeispiel.py
@@ -1,24 +1,25 @@
+# Einbinden der Klassendefinition
 from RemoteControl import RemoteControl
 
-# Instanziierung
-
-rc1 = RemoteControl(10)
-print(f"{rc1.volume=}")
-
-rc2 = RemoteControl(20)
-print(f"{rc2.volume=}")
-print(f"{rc2.programs[0]=}")
-
-
-for i in range(0,10):
-    rc1.plus()
-print(f"{rc1.volume=}")
-
-rc2.minus()
-rc2.minus()
-rc2.minus()
-print(f"{rc2.volume=}")
-
-
-print(f"{rc2.program[0]=}")
-
+# Klasse RemoteControl instanziieren - mit 5 Programmplätzen
+rc = RemoteControl(5)
+# aktuellen Programmplatz: Programmname setzen
+rc.setProgramName("ARD")
+# aktuellen Programmplatz: Programmnummer und Programmname ausgeben
+print(rc)
+# gehe 1 Sender weiter
+rc.nextProgram()
+# aktuellen Programmplatz: Programmnummer und Programmname ausgeben
+print(rc)
+# aktuellen Programmplatz: Programmname setzen
+rc.setProgramName("ZDF")
+# aktuellen Programmplatz: Programmnummer und Programmname ausgeben
+print(rc)
+# rc.__program[3] = "RTL"
+# führt zu AttributeError: 'RemoteControl' object has no attribute '__program'
+rc.plus()
+rc.plus()
+# aktuellen Programmplatz: Programmnummer und Programmname ausgeben
+print(rc)
+rc.minus()
+print(rc)
--- Unterricht_Aufgaben/classes/RemoteControl_Muster/RemoteControl.py
+++ Unterricht_Aufgaben/classes/RemoteControl_Muster/RemoteControl.py
@@ -1,33 +1,46 @@
-# Definition der Klasse RemoteControl
+# Klasse RemoteControl definieren
 class RemoteControl:
-    # Definition der Konstruktormethode
+    # Konstruktor, dem die maximale Anzahl an Programmspeicherplätzen
+    # zur Initialisierung übergeben wird
     def __init__(self, num_programs):
-        self.volume = 5
-
-        self.programs = []
+        self.__programs = []
+        # Initialisierung der Programmspeicherplätze
         for i in range(0, num_programs):
-            self.programs += ["Prog"+str(i+1)]
-
-        self.curr_program_number = 0
-
-
+            self.__programs += ["Prog " + str(i+1)]
+        self.__current_program_number = 0
+        self.__volume = 0
+        
+    # Methode zur Benennung des aktuellen Programms
+    def setProgramName(self, name):
+        self.__programs[self.__current_program_number] = name
+        
+    # Methode für den Wechsel zum nächsten Programm
+    def nextProgram(self):
+    # Gehe zum nächsten Programm, wenn noch nicht das Ende der Liste erreicht ist
+        if self.__current_program_number < len(self.__programs) - 1:
+            self.__current_program_number += 1
+        # sonst beginne wieder beim ersten Programm
+        else:
+            self.__current_program_number = 0
+            
         # Definition der Methode zum Erhöhen der Lautstärke
     def plus(self):
-        if self.volume >= 10:
+        if self.__volume >= 10:
             print("Lautstärke lässt sich nicht weiter erhöhen")
         else:
-            self.volume += 1
+            self.__volume += 1
 
     # Definition der Methode zum Verringern der Lautstärke
     def minus(self):
-        if self.volume > 0:
-            self.volume -= 1
+        if self.__volume > 0:
+            self.__volume -= 1
         else:
             print("Lautstärke ist bereits 0")
-
-    def setProgramName(self, name):
-        self.programs[self.curr_program_number] = name
-
-    def nextProgram(self):
-        self.curr_program_number += 1
     
+    # Methode zum Ausgeben des aktuellen Programms
+    # mit Nummer, Name und aktueller Lautstärke
+
+    def __str__ (self):
+        return (f"Nr {str(self.__current_program_number):>2} "
+                 f"Name {str(self.__programs[self.__current_program_number]):>6} "
+                 f"Lautstärke {str(self.__volume):>2} ")
