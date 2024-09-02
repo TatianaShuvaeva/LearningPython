cbbce16e778c005b548e6b6319e699fa61b6656c
TatianaShuvaeva <talisman1982@gmail.com>
2024-09-02 18:05:50 +0000

    Refactoring

---
+++ Unterricht_Aufgaben/classes/class_fahrzeug/lkw.py
@@ -0,0 +1,10 @@
+from fahrzeug import Fahrzeug
+
+
+class LKW(Fahrzeug):
+    def __init__(self, marke: str, baujahr: int, km_stand: float, farbe: str, gewichtklasse: float):
+        super().__init__(marke, baujahr, km_stand, farbe)
+        self.__gewichtklasse = gewichtklasse
+
+    def __str__(self):
+        return f"{super().__str__()}, Gewichtklasse - {self.__gewichtklasse}t"
---
+++ Unterricht_Aufgaben/classes/class_fahrzeug/pkw.py
@@ -0,0 +1,10 @@
+from fahrzeug import Fahrzeug
+
+
+class PKW(Fahrzeug):
+    def __init__(self, marke: str, baujahr: int, km_stand: float, farbe: str, kofferraumvolumen: int):
+        super().__init__(marke, baujahr, km_stand, farbe)
+        self.__kofferraumvolumen = kofferraumvolumen
+
+    def __str__(self):
+        return f"{super().__str__()}, Kofferraumvolumen - {self.__kofferraumvolumen}"
