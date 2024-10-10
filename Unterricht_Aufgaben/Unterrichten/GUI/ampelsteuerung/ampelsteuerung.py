dbc686e249a8636eaaa42551f125d66aa794325d
TatianaShuvaeva <talisman1982@gmail.com>
2024-10-10 18:20:00 +0000

    abstractproperty, Klasse Fussgängerampel hinzugefügt

--- Unterricht_Aufgaben/Unterrichten/GUI/ampelsteuerung/ampelsteuerung.py
+++ Unterricht_Aufgaben/Unterrichten/GUI/ampelsteuerung/ampelsteuerung.py
@@ -1,15 +1,46 @@
 from abc import ABC, abstractmethod
+from typing import List
+
 
 class Ampeln(ABC):
-    
+
     @abstractmethod
     def umschalten(self):
         pass
-    
+
+    @property
+    @abstractmethod
+    def farbe(self) -> str:
+        pass
+
+
 class AutoAmpel(Ampeln):
     def __init__(self):
-        self.farbe = ["rot", "rot-gelb", "gelb","grün"]
-    
-    def umschalten_AutoAmpel(self):
-        
-        
+        self.farben: List[str] = ["rot", "rot-gelb", "gelb", "grün"]
+        self._index: int = 0
+
+    def umschalten(self):
+        self._index += 1
+        if self._index > len(self.farben)-1:
+            self._index = 0
+
+    @property
+    def farbe(self) -> str:
+        farbe = self.farben[self._index]
+        return farbe
+
+
+class FussgaengerAmpel(Ampeln):
+    def __init__(self):
+        self.farben: List[str] = ["rot", "grün"]
+        self._index: int = 0
+
+    def umschalten(self):
+        self._index += 1
+        if self._index > len(self.farben)-1:
+            self._index = 0
+
+    @property
+    def farbe(self) -> str:
+        farbe = self.farben[self._index]
+        return farbe
--- Unterricht_Aufgaben/Unterrichten/GUI/ampelsteuerung/beispiel_ampelsteuerung.py
+++ Unterricht_Aufgaben/Unterrichten/GUI/ampelsteuerung/beispiel_ampelsteuerung.py
@@ -1,5 +1,15 @@
-from Unterricht_Aufgaben.Unterrichten.GUI.ampelsteuerung.ampelsteuerung import Ampeln, AutoAmpel
+from Unterricht_Aufgaben.Unterrichten.GUI.ampelsteuerung.ampelsteuerung import Ampeln, AutoAmpel, FussgaengerAmpel
 
 autoampel = AutoAmpel()
-print(autoampel.umschalten_AutoAmpel())
+fussgaengerampel = FussgaengerAmpel()
 
+
+for i in range(0, len(autoampel.farben)+1):
+    print(autoampel.farbe)
+    autoampel.umschalten()
+
+print("--------")   
+ 
+for i in range(0, len(fussgaengerampel.farben)+1):
+    print(fussgaengerampel.farbe)
+    fussgaengerampel.umschalten()
