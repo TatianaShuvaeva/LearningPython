df1c3b5e3c5029a5b9aa436dc23cc1dc65eb24b5
TatianaShuvaeva <talisman1982@gmail.com>
2024-10-16 15:17:12 +0200

    Methode Spiler wechseln hinzugefügt

--- Unterricht_Aufgaben/Unterrichten/GUI/tictactoe/cl_brett_ttt.py
+++ Unterricht_Aufgaben/Unterrichten/GUI/tictactoe/cl_brett_ttt.py
@@ -11,32 +11,41 @@ class TicTacToe:
         self.spielfeld.append(['4', '5','6'])
         self.spielfeld.append(['7', '8','9'])
         self.gewinnkombinationen = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
+        self.aktueller_spieler = 'x'
         
     def get(self) -> List[List[str]]:
         return self.spielfeld
+    
+    def spieler_wechseln(self):
+        if self.aktueller_spieler == 'x':
+            self.aktueller_spieler = 'o'
+        else:
+            self.aktueller_spieler = 'x'
 
     def set(self, nummer: int) -> None:
         if nummer == 1:
-            self.spielfeld[0][0] = 'x'
+            self._set_feld(0,0) 
         if nummer == 2:
-            self.spielfeld[0][1] = 'x'
+            self._set_feld(0,1) 
         if nummer == 3:
-            self.spielfeld[0][2] = 'x'
+            self._set_feld(0,2)
         if nummer == 4:
-            self.spielfeld[1][0] = 'x'
+            self._set_feld(1,0) 
         if nummer == 5:
-            self.spielfeld[1][1] = 'x'
+            self._set_feld(1,1) 
         if nummer == 6:
-            self.spielfeld[1][2] = 'x'
+            self._set_feld(1,2) 
         if nummer == 7:
             self._set_feld(2,0)
         if nummer == 8:
-             self._set_feld(2,1)
+            self._set_feld(2,1)
         if nummer == 9:
-             self._set_feld(2,2)
+            self._set_feld(2,2)
 
     def _set_feld(self, zeile, spalte):
-        if self.spielfeld[zeile][spalte] != "x":
-            self.spielfeld[zeile][spalte] = 'x'
+        
+        if self.spielfeld[zeile][spalte] != "x" and self.spielfeld[zeile][spalte] != "o":
+            self.spielfeld[zeile][spalte] = self.aktueller_spieler
+            self.spieler_wechseln()
         else:
             raise Exception("Diese Zelle ist bereits gefüllt")
