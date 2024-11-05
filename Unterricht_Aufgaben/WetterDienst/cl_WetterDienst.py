611586214ea0a99995655595404905301b40ce87
TatianaShuvaeva <talisman1982@gmail.com>
2024-11-05 19:39:03 +0000

    self._base_url ändert 2

--- Unterricht_Aufgaben/WetterDienst/cl_WetterDienst.py
+++ Unterricht_Aufgaben/WetterDienst/cl_WetterDienst.py
@@ -5,9 +5,11 @@ import json
 
 class WetterDienst:
     def __init__(self, api_key: str):
+        self._api_key = api_key
+        # https://example.com/path/to/page?name=ferret&color=purple
         self._base_url = (
-            "https://api.openweathermap.org/data/2.5/weather?"
-            "appid=" + api_key +
+            "https://api.openweathermap.org/data/2.5/weather"
+            "?appid={api_key}"
             "&q={stadt_name}"
             "&units=metric"
         )
@@ -15,8 +17,7 @@ class WetterDienst:
     @cache
     def get_temperatur(self, stadt: str) -> float:
 
-        complete_url = self._base_url.format(stadt_name=stadt)
-
+        complete_url = self._base_url.format(stadt_name=stadt, api_key=self._api_key)
         response = requests.get(complete_url)
         x = response.json()
         if x["cod"] != 200:
